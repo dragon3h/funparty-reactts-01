916a125cda36fc1d5e72cf7e7580faf3903cfb64
dragon3hgmx <dragon3h@gmx.com>
2020-10-02 12:42:59 -0400

    removed custom eslint config files

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import * as renderer from 'react-test-renderer';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const mainScreen = renderer
+      .create(
+          <App/>
+      )
+      .toJSON();
+  
+  expect(mainScreen).toMatchSnapshot();
 });
+
+// it('renders without crashing', () => {
+//   const tree = ren
+// });
