4c4488a591937a66a3253d12d9756c066f418d6f
dragon3hgmx <dragon3h@gmx.com>
2020-10-08 08:38:27 -0400

    added Lodash and handleSubmit

--- src/components/login/login.component.tsx
+++ src/components/login/login.component.tsx
@@ -1,20 +1,42 @@
 import React from 'react';
+import _ from 'lodash';
 
 import './login.styles.scss';
 import FormInput from '../shared/form-input/form-input.component';
 import CustomBtn from '../shared/custom-button/custom-button.component';
+import users from '../assets/user.json';
 
 type LoginProps = {};
 
 type LoginState = {
   email: string,
-  password: string
+  password: string,
+  isAuth: boolean,
 };
 
 class Login extends React.Component<LoginProps, LoginState> {
   state = {
     email: '',
     password: '',
+    isAuth: false
+  };
+  
+  handleSubmit = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    const email = this.state.email;
+    const pass = this.state.password;
+    
+    _.each(users, (user: { id: number, email: string, password: string }) => {
+      if (user.email === email && user.password === pass) {
+        this.setState((state) => {
+          const result = {
+            ...state,
+            isAuth: true
+          }
+          return result;
+        });
+      }
+    });
   };
   
   handleChange = (event: React.FormEvent<HTMLInputElement>): void => {
@@ -30,11 +52,14 @@ class Login extends React.Component<LoginProps, LoginState> {
   render() {
     return (
         <div className="login">
-          <FormInput handleChange={this.handleChange} label={'Email'} name="email" value={this.state.email}
-                     attrType={'email'} required/>
-          <FormInput handleChange={this.handleChange} label={'Password'} name="password" value={this.state.password}
-                     attrType={'password'}/>
-          <CustomBtn type="submit">Login</CustomBtn>
+          <h1>Login</h1>
+          <form onSubmit={this.handleSubmit}>
+            <FormInput handleChange={this.handleChange} label={'Email'} name="email" value={this.state.email}
+                       attrType={'email'} required/>
+            <FormInput handleChange={this.handleChange} label={'Password'} name="password" value={this.state.password}
+                       attrType={'password'}/>
+            <CustomBtn type="submit">Login</CustomBtn>
+          </form>
         </div>
     );
   }
